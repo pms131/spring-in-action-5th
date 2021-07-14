8552f10d0f1061bc54b7fbba30e3bbeb945d44d7
박문수 <sportssk@naver.com>
2021-07-14 21:50:33 +0900

    ch 04 - add spring security configure (JDBC 방식)

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -1,19 +1,22 @@
 package tacos.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-/*
- * InMemory 기반 인증 방식
- * 장점 : 테스트 목적이나 간단한 애플리케이션에는 편하다
- * 단점 : 사용자 추가, 삭제, 변경을 해야한다면 보안 구성 코드를 변경ㅎ나 후 애플리케이션을 다시 빌드, 배포, 설치해야 한다.
- *
+
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -25,7 +28,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .httpBasic()
         ;
     }
-
+/*
+ * InMemory 기반 인증 방식
+ * 장점 : 테스트 목적이나 간단한 애플리케이션에는 편하다
+ * 단점 : 사용자 추가, 삭제, 변경을 해야한다면 보안 구성 코드를 변경ㅎ나 후 애플리케이션을 다시 빌드, 배포, 설치해야 한다.
+ *
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
@@ -39,4 +46,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 ;
     }
     */
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(
+                        "select username, password, enabled from users " +
+                        "where username = ?")
+                .authoritiesByUsernameQuery(
+                        "select username, authority from authorities " +
+                        "where username = ?")
+                .passwordEncoder(new NoEncodingPasswordEncoder())
+        ;
+    }
 }
